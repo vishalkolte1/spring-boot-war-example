d353518688daa12bbcf3e0d287a7009560658e7c
vishalkolte1 <114715944+vishalkolte1@users.noreply.github.com>
2023-07-17 19:17:32 +0530

    Update HelloWorldApplication1.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World....This is My First Devops Project!";
+        return "Hello World....This is My First Devops Project...!";
     }
 
 }
